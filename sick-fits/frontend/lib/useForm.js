8cfaee6ce2809e388e5de473399eaed46f8f7685
Owen Olabode <oladapo2011@gmail.com>
2025-09-26 18:31:02 +0100

    update

--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -4,16 +4,34 @@ export default function useForm(initial = {}) {
   // create a state object for our inputs
   const [inputs, setInputs] = useState(initial);
   // create a function to handle changes
-  function handleChange(event) {
-    // const { name, value } = event.target;
+  function handleChange(e) {
+    let { name, value, type } = e.target;
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+    if (type === 'file') {
+      value[0] = e.target.files;
+    }
     setInputs({
       ...inputs,
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
   }
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
   // return the inputs and the handleChange function
-  //   return {
-  //     inputs,
-  //     handleChange,
-  //   };
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
 }
