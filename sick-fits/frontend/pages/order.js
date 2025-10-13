185a7c23a9834bc18ddd8362ffa86de2c28146d2
Owen Olabode <oladapo2011@gmail.com>
2025-10-13 21:42:44 +0100

    update

--- sick-fits/frontend/pages/order.js
+++ sick-fits/frontend/pages/order.js
@@ -1,11 +1,7 @@
-import Pagination from '../components/Pagination';
-import Products from '../components/Products';
-
 export default function OrderPage() {
   return (
     <div>
-      <Pagination />
-      <Products />
+      <p>order</p>
     </div>
   );
 }
