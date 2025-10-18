a14ea7ac79606cf4f14e94aff57fc4a951601c79
Owen Olabode <oladapo2011@gmail.com>
2025-10-18 23:24:22 +0100

    SignIn component

---
+++ sick-fits/frontend/components/SignIn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Form from './styles/Form';
+
+export default function SignIn() {
+  return (
+    <Form>
+      <h2>Sign Into Your Account</h2>
+    </Form>
+  );
+}
