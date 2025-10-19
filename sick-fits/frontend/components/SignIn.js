71a5f16be894797959918cbfc4d99d896d7465d6
Owen Olabode <oladapo2011@gmail.com>
2025-10-19 23:40:16 +0100

    update

--- sick-fits/frontend/components/SignIn.js
+++ sick-fits/frontend/components/SignIn.js
@@ -1,10 +1,72 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
 import Form from './styles/Form';
+import useForm from '../lib/useForm';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    authenticateUserWithPassword(email: $email, password: $password) {
+      ... on UserAuthenticationWithPasswordSuccess {
+        item {
+          id
+          email
+          name
+        }
+      }
+    }
+  }
+`;
 
 export default function SignIn() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+  });
+
+  const [signin, { loading, error }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+
+    // refetch the currently logged in user
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault(); // Prevent the form from submitting
+    const res = await signin();
+    console.log(res);
+    resetForm();
+
+    // Send the email and password to the graphqlAPI
+  }
   return (
-    <Form>
-      <h2>Sign Into Your Account</h2>
+    <Form method="POST" onSubmit={handleSubmit}>
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            autoComplete="current-password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In</button>
+      </fieldset>
     </Form>
   );
 }
