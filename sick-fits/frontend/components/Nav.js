ef359aff0960249fdd4acf527942092b994b39a8
Owen Olabode <oladapo2011@gmail.com>
2025-10-22 14:37:28 +0100

    signout component

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
+import SignOut from './SignOut';
 
 export default function Nav() {
   const user = useUser();
@@ -14,6 +15,7 @@ export default function Nav() {
           <Link href="/order">Orders</Link>
           <Link href="/sell">Sell</Link>
           <Link href="/account">Account</Link>
+          <SignOut />
         </>
       )}
       {!user && (
