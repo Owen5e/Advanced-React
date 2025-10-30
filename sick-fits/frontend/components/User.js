3a468e15778a5b6178d8984fd672dd6564677cfb
Owen Olabode <oladapo2011@gmail.com>
2025-10-30 22:51:47 +0100

    Cart - Displaying Items in a Custom Component

--- sick-fits/frontend/components/User.js
+++ sick-fits/frontend/components/User.js
@@ -9,12 +9,36 @@ export const CURRENT_USER_QUERY = gql`
         email
         name
         # query the cart items for the current user
+        cart {
+          id
+          quantity
+          product {
+            id
+            price
+            name
+            description
+            photo {
+              image {
+                publicUrlTransformed
+              }
+            }
+          }
+        }
       }
     }
   }
 `;
 
 export function useUser() {
-  const { data } = useQuery(CURRENT_USER_QUERY);
+  const { data, error, loading } = useQuery(CURRENT_USER_QUERY);
+
+  if (error) {
+    console.error('Error fetching user:', error);
+  }
+
+  if (loading) {
+    console.log('Loading user data...');
+  }
+
   return data?.authenticatedItem;
 }
